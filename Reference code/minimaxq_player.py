28cf3ec5e83af02a7a966fff977b1b1aaf518246
Prasanth Sengadu Suresh <prasuchit@gmail.com>
2020-04-21 13:22:09 -0400

    abcd

--- Reference code/minimaxq_player.py
+++ Reference code/minimaxq_player.py
@@ -4,27 +4,27 @@ from scipy.optimize import linprog
 class MinimaxQPlayer:
 
     def __init__(self, numStates, numActionsA, numActionsB, decay, expl, gamma):
-        self.decay = decay
-        self.expl = expl
-        self.gamma = gamma
-        self.alpha = 1
-        self.V = np.ones(numStates)
-        self.Q = np.ones((numStates, numActionsA, numActionsB))
-        self.pi = np.ones((numStates, numActionsA)) / numActionsA
+        self.decay = decay  # Learning rate decay
+        self.expl = expl    # Epsilon (how often the agent takes a random action)
+        self.gamma = gamma  # Discount factor
+        self.alpha = 1      # Learning rate
+        self.V = np.ones(numStates) # State value
+        self.Q = np.ones((numStates, numActionsA, numActionsB)) # Q value
+        self.pi = np.ones((numStates, numActionsA)) / numActionsA   # Policy
         self.numStates = numStates
         self.numActionsA = numActionsA
         self.numActionsB = numActionsB
-        self.learning = True
+        self.learning = True    # Stop learning or not
 
     def chooseAction(self, state, restrict=None):
-        if self.learning and np.random.rand() < self.expl:
+        if self.learning and np.random.rand() < self.expl:  # Pick random action if learning is true and within epsilon prob
             action = np.random.randint(self.numActionsA)
-        else:
+        else:   # Else, go with the policy
             action = self.weightedActionChoice(state)
         return action
 
-    def weightedActionChoice(self, state):
-        rand = np.random.rand()
+    def weightedActionChoice(self, state):  # Chooses random value that matches with an action from the policy
+        rand = np.random.rand()             # As policy distb changes, we become more biased towards certain actions over others
         cumSumProb = np.cumsum(self.pi[state])
         action = 0
         while rand > cumSumProb[action]:
@@ -32,18 +32,19 @@ class MinimaxQPlayer:
         return action
 
     def getReward(self, initialState, finalState, actions, reward, restrictActions=None):
-        if not self.learning:
+        if not self.learning:   
             return
         actionA, actionB = actions
         self.Q[initialState, actionA, actionB] = (1 - self.alpha) * self.Q[initialState, actionA, actionB] + \
             self.alpha * (reward + self.gamma * self.V[finalState])
         self.V[initialState] = self.updatePolicy(initialState)  # EQUIVALENT TO : min(np.sum(self.Q[initialState].T * self.pi[initialState], axis=1))
+        # self.V[initialState] = max(np.sum(self.Q[initialState].T * self.pi[initialState], axis=1))
         self.alpha *= self.decay
 
     def updatePolicy(self, state, retry=False):
         c = np.zeros(self.numActionsA + 1)
         c[0] = -1
-        A_ub = np.ones((self.numActionsB, self.numActionsA + 1))
+        A_ub = np.ones((self.numActionsB, self.numActionsA + 1))  # I believe this should be (numPlayers, numActionsA + 1)
         A_ub[:, 1:] = -self.Q[state].T
         b_ub = np.zeros(self.numActionsB)
         A_eq = np.ones((1, self.numActionsA + 1))
@@ -53,14 +54,18 @@ class MinimaxQPlayer:
 
         res = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
 
+        # arr = [self.weightedActionChoice(state),max(np.sum(self.Q[state].T * self.pi[state], axis=1))]
+        # self.pi[state] = arr[np.argmax(arr)]
+
         if res.success:
+            print("Policy updated with (state,action): ",state, ",", res.x[1:])
             self.pi[state] = res.x[1:]
         elif not retry:
             return self.updatePolicy(state, retry=True)
         else:
             print("Alert : %s" % res.message)
             return self.V[state]
-
+        print("Returning state value: ", res.x[0])
         return res.x[0]
 
     def policyForState(self, state):
